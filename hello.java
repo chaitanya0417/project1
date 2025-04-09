ca44d0ace51d37afb9e7cb7a10d611662644c00a
chaitanya0417 <chaitanyashinde000@gmail.com>
2025-04-09 15:45:41 +0530

    first commit

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        // Print "Hello" to the console
+        System.out.println("Hello");
     }
 }
+
